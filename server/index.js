3a6b0512579ef1c82f00250607c5b89dc7aab341
kylwllms <kayla.joann96@gmail.com>
2022-04-15 13:35:37 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+const inspiration = [
+  " You're doing great!",
+  "You're on the right track!",
+  "You can do it!",
+  "Keep it up!"
+] 
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+    "I see success in your future.",
+    "You will soon be visited by a stranger.",
+    "Nothing is more exciting than a sense of possibility.",
+    "Your close to success keep trying.",
+    "Trust your instints."
+  ];
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+});
+
+app.get("/api/quotes", (req, res) => {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  let randomQuote = quotes[randomIndex];
+
+  res.status(200).send(randomQuote);
+});
+
+
+
+app.listen(4000, () => console.log("Server running on 4000"));
